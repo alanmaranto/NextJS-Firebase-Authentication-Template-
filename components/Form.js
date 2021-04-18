c7653e8a47cd20c3881f51dbfdf80dfab4720062
alan <alanarostegui@hotmail.com>
2021-04-18 00:55:03 -0500

    login & register state

---
+++ components/Form.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Form = ({
+  email,
+  password,
+  setEmail,
+  setPassword,
+  btnName,
+  handleSubmit,
+}) => (
+  <div className="col-md-6">
+    <div className="form-group">
+      <label htmlFor="">Email</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        className="form-control"
+      />
+      <small>We'll never share your email</small>
+    </div>
+    <div className="form-group">
+      <label htmlFor="">Password</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        className="form-control"
+      />
+    </div>
+
+    <div>
+      <button className="btn btn-primary" onClick={handleSubmit}>
+        {btnName}
+      </button>
+    </div>
+  </div>
+);
+
+export default Form;
--- pages/login.js
+++ pages/login.js
@@ -1,51 +1,39 @@
 import React, { useState } from "react";
 import firebase from "../firebase";
 import { useRouter } from "next/router";
+import Form from "../components/Form";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
 
   const router = useRouter();
 
   const register = () => {};
   const login = () => {};
 
-  const renderForm = (btnName) => (
-    <div className="col-md-6">
-      <div className="form-group">
-        <label htmlFor="">Email</label>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          className="form-control"
-        />
-        <small>We'll never share your email</small>
-      </div>
-      <div className="form-group">
-        <label htmlFor="">Password</label>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          className="form-control"
-        />
-      </div>
-
-      <div>
-        <button className="btn btn-primary" onClick={register}>
-          {btnName}
-        </button>
-      </div>
-    </div>
-  );
-
   return (
     <div className="container">
       <h2 className="text-center pt-4 display-4">Login / Register</h2>
       <div className="row">
-        {renderForm("Login")} {renderForm("Register")}
+        <Form
+          email={loginEmail}
+          password={loginPassword}
+          setPassword={setLoginPassword}
+          setEmail={setLoginEmail}
+          btnName="Login"
+          handleSubmit={login}
+        />
+        <Form
+          email={registerEmail}
+          setEmail={setRegisterEmail}
+          password={registerPassword}
+          setPassword={setRegisterPassword}
+          btnName="Register"
+          handleSubmit={register}
+        />
       </div>
     </div>
   );
