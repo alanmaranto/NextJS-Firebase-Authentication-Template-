b361a27f8b46cc9c318832a69307c710c7c16c7e
alan <alanarostegui@hotmail.com>
2021-05-28 00:12:53 -0500

    axios config with base url and headers

--- components/FirebaseAuthState.js
+++ components/FirebaseAuthState.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext } from "react";
 import firebase from "../firebase";
 import { UserContext } from "../context/user/index";
 import { userTypes } from "../context/types";
-import axios from "axios";
+import { axiosAuth } from "../actions/axios";
 
 const FirebaseAuthState = ({ children }) => {
   const { dispatch } = useContext(UserContext);
@@ -14,37 +14,22 @@ const FirebaseAuthState = ({ children }) => {
           type: userTypes.LOGOUT,
         });
       } else {
-        try {
-          const { token } = await user.getIdTokenResult();
-          console.log(token);
-          // send this token to backend
-          const response = await axios.post(
-            "http://localhost:4000/current-user",
-            {},
-            {
-              headers: {
-                token,
-              },
-            }
-          );
+        const { token } = await user.getIdTokenResult();
+        console.log(token);
+        // send this token to backend
+        // backend will check if this token is valid (using firebase admin tool)
+        // if it's verified, you get the same user info in the backend too
+        // then you can decide to either save this user in your db or update the existing user
+        // then send the user info back to client
+        axiosAuth.post("/current-user", {}).then((response) => {
           console.log("response", response);
           dispatch({
             type: userTypes.LOGIN,
             payload: response.data,
           });
-        } catch (error) {
-          console.log(error);
-        }
-
-        // backend will check if this token is valid (using firebase admin tool)
-        // if it's verified, you get the same user info in the backend too
-        // then you can decide to either save this user in your db or update the existing user
-        // then send the user info back to client
-        /*         dispatch({
-          type: userTypes.LOGIN,
-          payload: user,
-        }); */
+        });
       }
+
     });
   }, []);
 
